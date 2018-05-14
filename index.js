8dbe1a96bd6d3010edd4e92e4b3bf68ec5a1b51c
Anna Perkowska <perkowska.a@gmail.com>
2018-05-14 10:04:33 +0200

    Add routes, css & presentational

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
+render(
+	<Provider store={store}>
+    	<Router routes={routes} history={hashHistory}>
+    		
+    	</Router>
+    	</div>
+    </Provider>,
+    document.getElementById('root')
+);
+
---
+++ presentational/contact.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => (
+	<div>
+		<h1>Search Country app</h1>
+		<h2>Route 66</h2>
+		<h2>Memphis<h2>
+		<h3>tel. 123 456 789</h3>
+	</div>
+);
+
+export default Contact;
